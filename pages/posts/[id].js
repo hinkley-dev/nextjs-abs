8edd54daa9778efee3aeabe50dd0fa30bfc090a4
mhinkley42 <mhinkley42@gmail.com>
2021-03-07 11:34:16 -0700

    Add title to the dynamically routing pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
@@ -11,6 +12,9 @@ export default function Post({ postData })
 {
 	return (
 		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
 			{postData.title}
 			<br />
 			{postData.id}
