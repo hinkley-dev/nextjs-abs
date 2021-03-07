f332612815acd03033d4b2241f670f53050edfa5
mhinkley42 <mhinkley42@gmail.com>
2021-03-07 11:10:09 -0700

    implement getStaticProps to load the blog post data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+/**
+ * Get the static data required for the Home page.
+ * The return of this is the input props to Home.
+ * Ran at build time in prod.
+ */
+export async function getStaticProps()
+{
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
 
 /**
  * Home page with the route '/'. Contains links to all other pages.
  */
-export default function Home()
+export default function Home({ allPostsData })
 {
   return (
     <Layout home>
@@ -18,6 +35,20 @@ export default function Home()
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
